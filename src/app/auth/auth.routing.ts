f68ee4453522dab1594c9d1de9d5772a7dece7b5
Jozielio Santiago <jozieliosantiago@gmail.com>
2017-12-02 18:32:21 -0300

    page cadastro created, page home - unexpected behavior fixed

--- src/app/auth/auth.routing.ts
+++ src/app/auth/auth.routing.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicialComponent } from './inicial/inicial.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 const routes: Routes = [
-  { path: '', component: InicialComponent }
+  { path: '', component: InicialComponent },
+  { path: 'cadastro', component: CadastroComponent }
 ];
 
 @NgModule({
