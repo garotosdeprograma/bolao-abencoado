fde550dd51f14db0eb2f837a5cb39d24ce580d17
Jozielio Santiago <jozieliosantiago@gmail.com>
2017-12-04 06:19:46 -0300

    page administrador created and created options menu, construction of administrador page options started

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin.routing';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin.component';
+import { AdministradorComponent } from './administrador/administrador.component';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { AdminComponent } from './admin.component';
   ],
   declarations: [
     HomeComponent,
-    AdminComponent
+    AdminComponent,
+    AdministradorComponent
   ]
 })
 export class AdminModule { }
--- src/app/admin/admin.routing.ts
+++ src/app/admin/admin.routing.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin.component';
+import { AdministradorComponent } from './administrador/administrador.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminComponent,
     children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'administrador', component: AdministradorComponent }
     ]
   }
 ];
