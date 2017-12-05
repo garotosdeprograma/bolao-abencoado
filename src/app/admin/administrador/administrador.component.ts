73b7cd779600d3698096e3dac8bafbc939cf5309
Jozielio Santiago <jozieliosantiago@gmail.com>
2017-12-05 09:22:00 -0300

    tables for listing and search of Campeonatos, Apostas, Usuários e Equipes created / Modal for edition and insertion of Campeonatos, Equipes created / view for insertion and edition of Jogos initiated

--- src/app/admin/administrador/administrador.component.ts
+++ src/app/admin/administrador/administrador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-administrador',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministradorComponent implements OnInit {
 
+  @ViewChild('jogos') Jogos: ElementRef;
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  mostrarJogos() {
+    this.Jogos.nativeElement.style.right = '0';    
+    this.Jogos.nativeElement.style.width = '100vw';
+    this.Jogos.nativeElement.style.display = 'block';        
+  }
+
+  voltar() {
+    this.Jogos.nativeElement.style.right = '-100px';    
+    this.Jogos.nativeElement.style.width = '0';
+    this.Jogos.nativeElement.style.display = 'none';
+  }
 }
