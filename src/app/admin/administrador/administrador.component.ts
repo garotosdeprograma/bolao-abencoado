1ed75482a8a34ed984aa7bfc8fac10188824f98f
Jozielio Santiago <jozieliosantiago@gmail.com>
2017-12-06 05:09:51 -0300

    view for insertion and edition of jogos created / view of ranking of the week created

--- src/app/admin/administrador/administrador.component.ts
+++ src/app/admin/administrador/administrador.component.ts
@@ -8,21 +8,67 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 export class AdministradorComponent implements OnInit {
 
   @ViewChild('jogos') Jogos: ElementRef;
-  
+  @ViewChild('ranking') Ranking: ElementRef;
+  @ViewChild('campeonatos') Campeonatos: ElementRef;  
+  @ViewChild('apostas') Apostas: ElementRef;
+  @ViewChild('usuarios') Usuarios: ElementRef;
+  @ViewChild('equipes') Equipes: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   mostrarJogos() {
-    this.Jogos.nativeElement.style.right = '0';    
-    this.Jogos.nativeElement.style.width = '100vw';
-    this.Jogos.nativeElement.style.display = 'block';        
+    // this.Jogos.nativeElement.style.right = '0';
+    // this.Jogos.nativeElement.style.width = '100vw';
+    this.Jogos.nativeElement.style.display = 'block';
   }
 
   voltar() {
-    this.Jogos.nativeElement.style.right = '-100px';    
-    this.Jogos.nativeElement.style.width = '0';
+    // this.Jogos.nativeElement.style.right = '-100px';
+    // this.Jogos.nativeElement.style.width = '0';
     this.Jogos.nativeElement.style.display = 'none';
   }
+
+  rankingSemana() {
+    this.Ranking.nativeElement.style.display = 'block';
+    this.Campeonatos.nativeElement.style.display = 'none';
+    this.Apostas.nativeElement.style.display = 'none';
+    this.Usuarios.nativeElement.style.display = 'none';
+    this.Equipes.nativeElement.style.display = 'none';
+  }
+
+  campeonato() {
+    this.Ranking.nativeElement.style.display = 'none';
+    this.Campeonatos.nativeElement.style.display = 'block';
+    this.Apostas.nativeElement.style.display = 'none';
+    this.Usuarios.nativeElement.style.display = 'none';
+    this.Equipes.nativeElement.style.display = 'none';
+  }
+
+  aposta() {
+    this.Ranking.nativeElement.style.display = 'none';    
+    this.Campeonatos.nativeElement.style.display = 'none';
+    this.Apostas.nativeElement.style.display = 'block';
+    this.Usuarios.nativeElement.style.display = 'none';
+    this.Equipes.nativeElement.style.display = 'none';
+  }
+
+  usuario() {
+    this.Ranking.nativeElement.style.display = 'none';    
+    this.Campeonatos.nativeElement.style.display = 'none';
+    this.Apostas.nativeElement.style.display = 'none';
+    this.Usuarios.nativeElement.style.display = 'block';
+    this.Equipes.nativeElement.style.display = 'none';
+  }
+
+  equipe() {
+    this.Ranking.nativeElement.style.display = 'none';    
+    this.Campeonatos.nativeElement.style.display = 'none';
+    this.Apostas.nativeElement.style.display = 'none';
+    this.Usuarios.nativeElement.style.display = 'none';
+    this.Equipes.nativeElement.style.display = 'block';
+  }
+
 }
