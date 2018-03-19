246ba16779ee073ca49438b9041166a5c608f9d0
Daouda Traore <daouda@daouda.daouda>
2018-03-19 17:15:00 -0300

    switched api endpoint to heroku

--- src/app/constant/api.ts
+++ src/app/constant/api.ts
@@ -1,5 +1,5 @@
 // export const URL_API = 'http://localhost:8000/';
 
-// export const URL_API = 'https://api-bolao-abencoado.herokuapp.com/';
+export const URL_API = 'https://api-bolao-abencoado.herokuapp.com/';
 
-export const URL_API = 'http://ec2-107-23-56-74.compute-1.amazonaws.com/gamebol/api/public/';
+// export const URL_API = 'http://ec2-107-23-56-74.compute-1.amazonaws.com/gamebol/api/public/';
